17421ddab34cf3a0e2e926c45d4871d45ddc87ec
Do Hyung Kwon <thedavekwon@gmail.com>
2020-04-09 21:14:48 -0400

    Added table parser to print in bracketed notation Added argparse to parse input

--- CNFUtils.py
+++ CNFUtils.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def parse_cnf_grammar(cnf_path):
     with open(cnf_path, "r") as f:
         single_grammar = {}
@@ -15,3 +18,24 @@ def parse_cnf_grammar(cnf_path):
                     double_grammar[left] = []
                 double_grammar[left].append(right.split(" "))
     return single_grammar, double_grammar
+
+
+def pprint(table, single_grammar, words, i, j):
+    print(table[(i, j)])
+    for k in range(i, j):
+        print(single_grammar[words[k]], end=" ")
+    print("")
+
+
+def traverse_table(table, words, i, j, g):
+    ret = []
+    for entry in table[(i, j)]:
+        if entry[0] == g:
+            if len(entry) == 2:
+                return ["[{} {}]".format(g, words[i])]
+            else:
+                left = traverse_table(table, words, *entry[1])
+                right = traverse_table(table, words, *entry[2])
+                for l, r in list(itertools.product(left, right)):
+                    ret.append("[{} {} {}]".format(g, l, r))
+    return ret
--- CYKparser.py
+++ CYKparser.py
@@ -1,32 +1,68 @@
-from CNFUtils import parse_cnf_grammar
+import argparse
 import itertools
 
-CNF_PATH = "data/sampleGrammar.cnf"
+from CNFUtils import parse_cnf_grammar, traverse_table
+
+parser = argparse.ArgumentParser(description='Constituency Parser with CYK algorithms')
+parser.add_argument('--cnf', help='path of cnf grammar', type=str, required=True)
+args = parser.parse_args()
+CNF_PATH = args.cnf
 
 
-def cyk_parser(words, single_grammar, double_grammar):
+def cyk_recognizer(words, single_grammar, double_grammar):
     print(single_grammar)
     print(double_grammar)
     N = len(words)
     table = {}
     # generate table with appropriate index (upper triangle created for optimization)
-    for key in list(itertools.product(range(0, N+1), range(0, N+1))):
-        if key[0] > key[1]:
+    for key in list(itertools.product(range(0, N + 1), range(0, N + 1))):
+        if key[0] >= key[1]:
             continue
         table[key] = set()
-    for j in range(1, N+1):
-        for g in single_grammar[words[j-1]]:
-            table[(j-1, j)] = table[(j-1, j)].union({g})
-        for i in range(j-2, -1, -1):
-            for k in range(i+1, j):
+    for j in range(1, N + 1):
+        for g in single_grammar[words[j - 1]]:
+            table[(j - 1, j)] = table[(j - 1, j)].union({g})
+        for i in range(j - 2, -1, -1):
+            for k in range(i + 1, j):
                 for dg_key in double_grammar.keys():
                     dgs = double_grammar[dg_key]
                     for dg in dgs:
                         if dg[0] in table[(i, k)] and dg[1] in table[(k, j)]:
                             table[(i, j)] = table[(i, j)].union({dg_key})
-    return table
+    return 'S' in table[(0, N)]
+
+
+def cyk_parser(words, single_grammar, double_grammar):
+    N = len(words)
+    table = {}
+    # generate table with appropriate index (upper triangle created for optimization)
+    for key in list(itertools.product(range(0, N + 1), range(0, N + 1))):
+        if key[0] >= key[1]:
+            continue
+        table[key] = []
+    for j in range(1, N + 1):
+        for g in single_grammar[words[j - 1]]:
+            table[(j - 1, j)].append([g, (j - 1, j)])
+        for i in range(j - 2, -1, -1):
+            for k in range(i + 1, j):
+                for dg_key in double_grammar.keys():
+                    dgs = double_grammar[dg_key]
+                    for dg in dgs:
+                        if dg[0] in list(map(lambda x: x[0], table[(i, k)])) and \
+                                dg[1] in list(map(lambda x: x[0], table[(k, j)])):
+                            table[(i, j)].append([dg_key, (i, k, dg[0]), (k, j, dg[1])])
+    if 'S' in list(map(lambda x: x[0], table[(0, N)])):
+        return traverse_table(table, words, 0, N, 'S')
+    return None
+
 
 if __name__ == "__main__":
     single_grammar, double_grammar = parse_cnf_grammar(CNF_PATH)
-    words = "i book the flight from houston".split(" ")
-    cyk_parser(words, single_grammar, double_grammar)
+    words = input().split(" ")
+    print(words)
+    parsed = cyk_parser(words, single_grammar, double_grammar)
+    if parsed is None:
+        print("NO VALID PARSES")
+    else:
+        for p in parsed:
+            print(p)
